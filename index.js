0a569d126bbd974be398ac6d7a0507fbf7cb480c
olafnub <googlhoogle@gmail.com>
2024-06-12 17:01:43 -0500

    Add the demo documentation

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const baseAPI = process.env.BASE_API;
+
+fetch(`https://api.basescan.org/api
+?module=account
+&action=balance
+&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
+&tag=latest
+&apikey=YourApiKeyToken`)
+
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.log(err))
+// https://api.basescan.org/api
+//    ?module=account
+//    &action=balance
+//    &address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
+//    &tag=latest
+//    &apikey=YourApiKeyToken
