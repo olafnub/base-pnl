bc6b33596027e38b5ebec2ca0a9a2ee133a987f4
olafnub <googlhoogle@gmail.com>
2024-06-15 14:34:15 -0500

    Add express server and create example fetch

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-require('dotenv').config()
+const express = require("express")
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+require("dotenv").config();
 
 const baseAPI = process.env.BASE_API;
 const address = '0x4200000000000000000000000000000000000011';  
 
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.post("/base-api", (req, res) => {
+    const { address } = req.body;
+    fetch(`https://api.basescan.org/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${baseAPI}`)
+        .then(response => response.json())
+        .then(data => res.json(data))
+        .catch(err => {
+            console.error(err);
+            res.status(500).send("Error fetching data");
+        });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+// How express and PORT works: https://medium.com/@ralph1786/how-to-setup-an-express-server-5fd9cd9ae073
+// How rendering index.html works: https://gist.github.com/ryanoglesby08/1e1f49d87ae8ab2cabf45623fc36a7fe?permalink_comment_id=3620942
 fetch(`https://api.basescan.org/api?module=account
 &action=balance&address=${address}&tag=latest&apikey=${baseAPI}`)
 
