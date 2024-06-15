bee34c16e537392452b4f804cc764dfd999d2de1
ambeesa <adamkidaneee@gmail.com>
2024-06-15 13:42:04 -0500

    changes

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 require('dotenv').config()
 
 const baseAPI = process.env.BASE_API;
+const address = '0x4200000000000000000000000000000000000011';  
 
-fetch(`https://api.basescan.org/api
-?module=account
-&action=balance
-&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
-&tag=latest
-&apikey=${baseAPI}`)
+
+fetch(`https://api.basescan.org/api?module=account
+&action=balance&address=${address}&tag=latest&apikey=${baseAPI}`)
+
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => console.log('Balance:',data))
 .catch(err => console.log(err))
-// https://api.basescan.org/api
-//    ?module=account
-//    &action=balance
-//    &address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
-//    &tag=latest
-//    &apikey=YourApiKeyToken
+
+const blockNumber = 15841878; 
+fetch(`https://api.basescan.org/api?module=block&action=getblockreward&blockno=${blockNumber}&apikey=${baseAPI}`)
+  .then(res => res.json())
+  .then(data => console.log('Block Information:', data))
+  .catch(err => console.log(err));
+
+  const txHash = '0x0326787a723546e7f8c193dcd6bcb603ac7cfad97c0ebd5e34a5c0578359666b'; 
+fetch(`https://api.basescan.org/api?module=transaction&action=gettxreceiptstatus&txhash=${txHash}&apikey=${baseAPI}`)
+  .then(res => res.json())
+  .then(data => console.log('Transaction Receipt Status:', data))
+  .catch(err => console.log(err));
+
+
+  fetch(`https://api.basescan.org/api?module=proxy&action=eth_gasPrice&apikey=${baseAPI}`)
+  .then(res => res.json())
+  .then(data => console.log('Gas Price:', data))
+  .catch(err => console.log(err));
+
+  fetch(`https://api.basescan.org/api?module=account&action=txlistinternal&txhash=${txHash}&apikey=${baseAPI}`)
+  .then(res => res.json())
+  .then(data => console.log('Internal Transactions by Tx Hash:', data))
+  .catch(err => console.log(err));
