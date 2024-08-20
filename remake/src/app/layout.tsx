f1b0832373c553e9ba28879db8dc5fb76819a952
olafnub <googlhoogle@gmail.com>
2024-08-20 13:38:07 -0600

    Finish google anayltics and socials styling

--- remake/src/app/layout.tsx
+++ remake/src/app/layout.tsx
@@ -4,13 +4,14 @@ import "./globals.css";
 import NavBar from "./components/NavBar";
 import { WalletProvider } from "./context"
 import Socials from "./components/Socials"
-// import Head from "next/head"
-// import Script from "next/script"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "Wallet Finance",
   description: "Keep Track of Your Crypto Finance",
 };
+
+const GOOGLE_KEY = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
 
 export default function RootLayout({
   children,
@@ -19,33 +20,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <Head> */}
-      {/*  Google Tag Manager */}
-        {/* <Script async src='https://www.googletagmanager.com/gtm.js?id='/> */}
-          
-        {/* <Script dangerouslySetInnerHTML={
-          function(w: any,d: any,s: any,l: any,i: any){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.af+i+dl;f.parentNode.insertBefore(j,f);
-            }(window,document,'script','dataLayer','GTM-WF7HQMKT');
-        }/> */}
-        {/* End Google Tag Manager */}
-      {/* </Head> */}
+      <head>
+
+      {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-${GOOGLE_KEY}`}></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {
+            `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-${GOOGLE_KEY}');
+            `
+          }
+        </Script>
+      </head>
       <WalletProvider>
-      <body style={{height: "95vh"}}>
+      <body className="h-screen">
           <header>
             <NavBar /> 
           </header>
           <main>
             {children}
           </main>
-          <footer>
+          <footer className="place-content-end">
+            <Socials />
           </footer>
       </body>
       </WalletProvider>
-      <footer>
-        <Socials />
-      </footer>
     </html>
   );
 }
